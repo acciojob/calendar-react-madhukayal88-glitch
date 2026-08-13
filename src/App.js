34bd7544c53ad46613a0b2f6ed6e64c1bd937f57
kayalvizhi <madhukayal88@gmail.com>
2026-08-14 05:10:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,154 @@
-import React from 'react';
-import Calendar from './Calendar';
-import './styles.css';
+Skip to content
+acciojob
+calendar-react-madhukayal88-glitch
+Repository navigation
+Code
+Issues
+Pull requests
+Agents
+Actions
+Projects
+Wiki
+Security and quality
+3
+ (3)
+Insights
+Settings
+acciojob
+calendar-react-madhukayal88-glitch
+Public
+Go to file
+t
+T
+Name		
+madhukayal88-glitch
+madhukayal88-glitch
+Update App.css
+aebdbcd
+ · 
+last week
+public
+Initialize project using Create React App
+last week
+src
+Update App.css
+last week
+.gitignore
+Initialize project using Create React App
+last week
+README.md
+Initialize project using Create React App
+last week
+package-lock.json
+Initialize project using Create React App
+last week
+package.json
+Initialize project using Create React App
+last week
+Repository files navigation
+README
+Getting Started with Create React App
+This project was bootstrapped with Create React App.
 
-function App() {
-  return (
-    <div className="app-container">
-      <Calendar />
-    </div>
-  );
-}
+Available Scripts
+In the project directory, you can run:
 
-export default App;
+npm start
+Runs the app in the development mode.
+Open http://localhost:3000 to view it in your browser.
+
+The page will reload when you make changes.
+You may also see any lint errors in the console.
+
+npm test
+Launches the test runner in the interactive watch mode.
+See the section about running tests for more information.
+
+npm run build
+Builds the app for production to the build folder.
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.
+Your app is ready to be deployed!
+
+See the section about deployment for more information.
+
+npm run eject
+Note: this is a one-way operation. Once you eject, you can't go back!
+
+If you aren't satisfied with the build tool and configuration choices, you can eject at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except eject will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+
+You don't have to ever use eject. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+
+Learn More
+You can learn more in the Create React App documentation.
+
+To learn React, check out the React documentation.
+
+Code Splitting
+This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+
+Analyzing the Bundle Size
+This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
+
+Making a Progressive Web App
+This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
+
+Advanced Configuration
+This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
+
+Deployment
+This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
+
+npm run build fails to minify
+This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+About
+No description, website, or topics provided.
+Resources
+Readme
+Activity
+Custom properties
+Stars
+0 stars
+Watchers
+0 watching
+Forks
+0 forks
+Report repository
+Releases
+No releases published
+Create a new release
+Packages
+No packages published
+Publish your first package
+Contributors
+2
+ (2)
+@madhukayal88-glitch
+madhukayal88-glitchkayalvizhi
+@acciojob-tech-team-3
+acciojob-tech-team-3
+Languages
+JavaScript
+53.3%
+CSS
+33.7%
+HTML
+13%
+Generated from acciojob/calendar-react
+Footer
+© 2026 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Community
+Docs
+Contact
+Manage cookies
+Do not share my personal information
+ 
